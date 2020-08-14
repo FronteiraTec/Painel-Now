427b98d4ce4ca8e754afba92993162a7db17898a
mazarafa <arruda.rafa.ra@gmail.com>
2020-08-14 18:29:28 -0400

    adicionando icon de sair, acrecentando nas pages

--- app/includes/header.php
+++ app/includes/header.php
@@ -4,8 +4,8 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="../assets/img/dce-logo.png">
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css"/>
-    <title>Painel</title>   
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css" />
+    <title>Painel</title>
 </head>
 
 <body>
@@ -13,8 +13,8 @@
     <div id="mySidenav" class="sidenav">
         <nav class="navbar navbar-light bg-light">
             <a class="navbar-brand" href="#">
-                <img src="../assets/img/dce-logo.png"  style="padding-left: 40px;"  width="100" height="100" alt="">
-            </a> 
+                <img src="../assets/img/dce-logo.png" style="padding-left: 40px;" width="100" height="100" alt="">
+            </a>
         </nav>
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
         <a href="dashboard.php">Home</a>
@@ -27,4 +27,11 @@
     <div id="main">
 
         <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Painel</span>
+
+        <div class="deslogar">
+            <span id="sair"><a href="#"><img src="../assets/img/saida.png" width="32" alt="sair"></a></span>
+        </div>
+
     </div>
+
+    
