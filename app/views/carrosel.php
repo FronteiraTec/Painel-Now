5e949a8883b4d1b7092a2a27ee52f935824f744e
mazarafa <arruda.rafa.ra@gmail.com>
2020-08-13 23:09:08 -0400

    carrosel de troca de imagem, feito com sucesso

---
+++ app/views/carrosel.php
@@ -0,0 +1,16 @@
+<?php require_once("../includes/header.php") ?>
+
+    <div class="home-image">
+        <form action="#">
+            <div class="form-Himage">
+                <label for="exampleFormControlFile1">Anexar imagem</label>
+                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+
+        </form>
+
+    </div>
+
+<form action="#">
+
+    <?php require_once("../includes/footer.php") ?>
