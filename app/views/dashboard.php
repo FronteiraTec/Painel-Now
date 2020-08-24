697db246ec2875c081440b48668bf78182e10efa
mazarafa <arruda.rafa.ra@gmail.com>
2020-08-24 16:32:41 -0400

    ..

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -1,3 +1,15 @@
-<?php require_once("../includes/header.php"); ?>
+<?php  
+// session_start();
+    require_once("../includes/header.php"); 
+
+    require_once('../controller/verifica_login.php');
+?>
+
+
+<h2>Olá, <?php echo $_SESSION['usuario'];?></h2>
+<!-- implementar mais ideias para essa home -->
+
+<!-- <h2><a href="logout.php">Sair</a></h2> -->
+
 
 <?php require_once("../includes/footer.php"); ?>
